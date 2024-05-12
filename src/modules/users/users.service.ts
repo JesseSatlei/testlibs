77fb513fd6dcf0cc7ae1d37083e05e1324ad7b24
Jesse Satlei <jessesatlei@hotmail.com>
2024-05-12 16:38:35 -0300

    changed

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,26 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+const userTest = {
+  id: 1,
+  name: 'Fulano'
+};
 @Injectable()
 export class UsersService {
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return userTest;
   }
 
   findAll() {
-    return `This action returns all users`;
+    return [{userTest}];
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return userTest;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    return userTest;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return true;
   }
 }
